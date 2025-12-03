38ab85fc77e26dfd21b632bdd0e0f36c8e705472
Soffffan <baranova.sof@yandex.ru>
2025-12-03 11:02:46 +0300

    api_gateway proxy

---
+++ api_gateway/src/proxy/ordersProxy.js
@@ -0,0 +1,35 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const ordersProxy = createProxyMiddleware({
+  target: process.env.ORDERS_SERVICE_URL || 'http://localhost:8002',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/v1/orders': '/'
+  },
+  proxyTimeout: 10000,
+  timeout: 10000,
+  onProxyReq: (proxyReq, req, res) => {
+    console.log('  [PROXY] Forwarding request to users service');
+    console.log(`   Path: ${req.path} -> ${proxyReq.path}`);
+    
+    if (req.body) {
+      console.log('   Body detected, setting content...');
+      const bodyData = JSON.stringify(req.body);
+      proxyReq.setHeader('Content-Type', 'application/json');
+      proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+      proxyReq.write(bodyData);
+    }
+  },
+  onError: (err, req, res) => {
+    console.error('   [PROXY] Error:', err.message);
+    res.status(503).json({
+      success: false,
+      error: {
+        code: 'PROXY_ERROR',
+        message: 'Ошибка проксирования запроса'
+      }
+    });
+  }
+});
+
+module.exports = ordersProxy;
